006f5acb17cdcdc1173602e6ea9f4b753bc45cef
Chris <chris.ware.dev@gmail.com>
2021-08-27 18:26:32 -0700

    Intro Page Config w/ down pointing Arrow

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,24 @@
-import './intro.scss'
-
-import React from 'react'
+import React from "react";
+import "./intro.scss";
 
 export default function Intro() {
-    return (
-        <div className='intro' id='intro'>
-      
-        </div>
-    )
+  return (
+    <div className='intro' id='intro'>
+      <div className='left'>
+          <div className="imgContainer">
+              <img src="assets/man.png" alt="man" />
+          </div>
+      </div>
+      <div className='right'>
+          <div className="wrapper">
+              <h2>Hi There, I'm</h2>
+              <h1>Slim Shady</h1>
+              <h3>Freelance  <span>Designer</span></h3>
+          </div>
+          <a href="#portfolio">
+              <img src="assets/down.png" alt="" />
+          </a>
+      </div>
+    </div>
+  );
 }
