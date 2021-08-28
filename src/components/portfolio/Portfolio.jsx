bcf108b137fbeccb7debc5396b7697fb61533ffb
Chris <chris.ware.dev@gmail.com>
2021-08-28 00:25:05 -0700

    Dynamic Portfolio List

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,98 @@
-import './portfolio.scss'
+import "./portfolio.scss";
 
-import React from 'react'
+import React, { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 
 export default function Portfolio() {
-    return (
-        <div className='portfolio' id='portfolio'>
-       
+  const [selected, setSelected] = useState("featured");
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
+  return (
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img
+            src='https://media-exp1.licdn.com/dms/image/C4E0BAQHhbVLaNOTTfA/company-logo_200_200/0/1556848405410?e=2159024400&v=beta&t=wWw3x55m5NtiV9tjtr0uUVvV-Avz2lYcvysSn0ALzQ0'
+            alt=''
+          />
+          <h3>Banking App</h3>
         </div>
-    )
+        <div className='item'>
+          <img
+            src='https://media-exp1.licdn.com/dms/image/C4E0BAQHhbVLaNOTTfA/company-logo_200_200/0/1556848405410?e=2159024400&v=beta&t=wWw3x55m5NtiV9tjtr0uUVvV-Avz2lYcvysSn0ALzQ0'
+            alt=''
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img
+            src='https://media-exp1.licdn.com/dms/image/C4E0BAQHhbVLaNOTTfA/company-logo_200_200/0/1556848405410?e=2159024400&v=beta&t=wWw3x55m5NtiV9tjtr0uUVvV-Avz2lYcvysSn0ALzQ0'
+            alt=''
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img
+            src='https://media-exp1.licdn.com/dms/image/C4E0BAQHhbVLaNOTTfA/company-logo_200_200/0/1556848405410?e=2159024400&v=beta&t=wWw3x55m5NtiV9tjtr0uUVvV-Avz2lYcvysSn0ALzQ0'
+            alt=''
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img
+            src='https://media-exp1.licdn.com/dms/image/C4E0BAQHhbVLaNOTTfA/company-logo_200_200/0/1556848405410?e=2159024400&v=beta&t=wWw3x55m5NtiV9tjtr0uUVvV-Avz2lYcvysSn0ALzQ0'
+            alt=''
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img
+            src='https://media-exp1.licdn.com/dms/image/C4E0BAQHhbVLaNOTTfA/company-logo_200_200/0/1556848405410?e=2159024400&v=beta&t=wWw3x55m5NtiV9tjtr0uUVvV-Avz2lYcvysSn0ALzQ0'
+            alt=''
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img
+            src='https://media-exp1.licdn.com/dms/image/C4E0BAQHhbVLaNOTTfA/company-logo_200_200/0/1556848405410?e=2159024400&v=beta&t=wWw3x55m5NtiV9tjtr0uUVvV-Avz2lYcvysSn0ALzQ0'
+            alt=''
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
